fb07bd588d7c2541a6958b9128aee69a64fefb27
monosoul <andrei.nevedomskii@wolt.com>
2022-03-30 20:33:49 +0200

    Configure the app to actually run

--- root/app/src/main/kotlin/dev/monosoul/hexagonal/example/app/App.kt
+++ root/app/src/main/kotlin/dev/monosoul/hexagonal/example/app/App.kt
@@ -1,5 +1,30 @@
 package dev.monosoul.hexagonal.example.app
 
+import dev.monosoul.hexagonal.persistence.jooq.PersistenceConfig
+import dev.monosoul.hexagonal.persistence.jooq.jooqPersistenceModule
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+import java.lang.Runtime.getRuntime
+
+private val configurations = module {
+    single {
+        PersistenceConfig(
+            host = "localhost",
+            port = 6666,
+            database = "hexagonal",
+            schema = "public",
+            username = "hexagonal",
+            password = "hexagonal"
+        )
+    }
+}
+
 fun main() {
-    println(MessageUtils.getMessage())
+    startKoin {
+        modules(
+            configurations + jooqPersistenceModule
+        )
+    }.also {
+        getRuntime().addShutdownHook(Thread(it::close, "ShutdownHook"))
+    }
 }
