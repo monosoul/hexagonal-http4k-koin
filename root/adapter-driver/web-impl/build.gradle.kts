a4e0e1dd6ccced7043a45cf4a69b69a8a93ac02a
monosoul <andrei.nevedomskii@wolt.com>
2022-03-31 09:12:48 +0200

    Remove redundant dependency

--- root/adapter-driver/web-impl/build.gradle.kts
+++ root/adapter-driver/web-impl/build.gradle.kts
@@ -6,7 +6,6 @@ dependencies {
     implementation(project(":domain-api"))
 
     implementation("io.insert-koin:koin-core-jvm")
-    implementation("org.http4k:http4k-core")
     implementation("org.http4k:http4k-server-undertow")
     implementation("org.http4k:http4k-format-kotlinx-serialization")
 }
