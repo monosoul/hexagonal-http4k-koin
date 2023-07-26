93df94251045c64f1ba674693c585a2a0dc64b60
monosoul <andrei.nevedomskii@wolt.com>
2023-07-26 18:39:08 +0200

    Use the latest jooq codegen plugin version

--- root/adapter-driven/persistence-jooq/build.gradle.kts
+++ root/adapter-driven/persistence-jooq/build.gradle.kts
@@ -1,46 +1,34 @@
+import dev.monosoul.jooq.RecommendedVersions.FLYWAY_VERSION
+import dev.monosoul.jooq.RecommendedVersions.JOOQ_VERSION
 import org.jooq.meta.jaxb.ForcedType
 
 plugins {
     id("dev.monosoul.hexagonal.example.kotlin-library-conventions")
-    id("com.revolut.jooq-docker") version "0.3.7"
-}
-
-buildscript {
-    val jooqVersion by project.extra { "3.16.5" }
-    repositories {
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath("org.jooq:jooq-codegen:$jooqVersion")
-    }
+    id("dev.monosoul.jooq-docker") version "4.0.1"
 }
 
 dependencies {
     api(project(":domain-persistence-spi"))
 
     implementation("io.insert-koin:koin-core-jvm")
-    val jooqVersion: String by project.extra
-    implementation("org.jooq:jooq:$jooqVersion")
+    implementation("org.jooq:jooq:$JOOQ_VERSION")
 
-    val postgresJdbcVersion = "42.3.3"
-    jdbc("org.postgresql:postgresql:$postgresJdbcVersion")
+    val postgresJdbcVersion = "42.3.8"
+    jooqCodegen("org.postgresql:postgresql:$postgresJdbcVersion")
     implementation("org.postgresql:postgresql:$postgresJdbcVersion")
-    implementation("org.flywaydb:flyway-core:8.5.5")
+    implementation("org.flywaydb:flyway-core:$FLYWAY_VERSION")
     implementation("com.zaxxer:HikariCP:5.0.1")
-}
-
-jooq {
-    image {
-        tag = "13.4-alpine"
-    }
 }
 
 tasks {
     generateJooqClasses {
-        basePackageName = "dev.monosoul.hexagonal.persistence.jooq.generated"
-        excludeFlywayTable = true
-        customizeGenerator {
+        basePackageName.set("dev.monosoul.hexagonal.persistence.jooq.generated")
+        withContainer {
+            image {
+                name = "postgres:13.4-alpine"
+            }
+        }
+        usingJavaConfig {
             database.apply {
                 fun forcedType(
                     type: String,
